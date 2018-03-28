8c759773779333df25e1fac63a0e52ea052cb7dc
joshsamuelson <32172285+joshsamuelson@users.noreply.github.com>
2018-03-28 16:53:05 -0700

    Update filebeat_puppetserver.pp

--- site/profile/manifests/filebeat_puppetserver.pp
+++ site/profile/manifests/filebeat_puppetserver.pp
@@ -1,13 +1,2 @@
 class profile::filebeat_puppetserver {
-  elk::filebeat{'puppet':
-    prospectors => [ {
-    "type" => "log",
-    "paths" => [
-       "/var/log/puppetlabs/puppetserver/puppetserver.log.json",
-       "/var/log/puppetlabs/puppetserver/puppetserver-access.log.json",
-      ],
-     "json_keys_under_root" => true
-   } ],
-   logstash_server => 'elk.local',
-   }
 }
